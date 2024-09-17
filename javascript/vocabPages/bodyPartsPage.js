081495665da24c2fb6ef554260fc01efcd977bf1
d-crompton <danny.crompton@live.co.uk>
2024-09-17 09:53:28 +0800

    Creation of body parts JavaScript file

---
+++ javascript/vocabPages/bodyPartsPage.js
@@ -0,0 +1,32 @@
+// Vocab Objects - One per table
+const BodyPartsVocab = {
+  Head: "Kepala",
+  Neck: "Leher",
+  "Back of the Neck": "Tengkuk",
+  Bone: "Tulang",
+  Collarbone: "Tulang Selangka",
+  Shoulder: "Bahu",
+  "Shoulder Blade": "Tulang Belikat",
+  Chest: "Dada",
+  Stomach: "Perut",
+  "Belly Button": "Pusat <i>(Also means centre)</i>",
+  Arm: "Lengan",
+  Elbow: "Siku",
+  Wrist: "Pergelangan Tangan",
+  Hand: "Tangan",
+  Waist: "Pinggang",
+  Hips: "Pinggul",
+  Butt: "Punggung",
+  "Private Parts": "Kemaluan",
+  Thigh: "Peha",
+  Knee: "Lutut",
+  Calves: "Betis",
+  Shinbone: "Tulang Kering <i>(literally 'Dry Bone')</i>",
+  Ankle: "Pergelangan Kaki",
+  Foot: "Kaki",
+  Heel: "Tumit Kaki",
+  Toes: "Jari Kaki",
+};
+
+const facialFeaturesVocab = {};
+const fingersVocab = {};
