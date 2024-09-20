46b7786d54560b1c103720a506962dcda2d031e9
d-crompton <danny.crompton@live.co.uk>
2024-09-20 21:04:51 +0800

    Creation of introductions page HTML and JS doc

---
+++ javascript/vocabPages/introductionsPage.js
@@ -0,0 +1,25 @@
+// Vocab Object
+const greetingsVocab = {
+  "Good Morning": "Selamat Pagi",
+  "Good Afternoon": "Selamat Tengah Hari",
+  "Good Evening": "Selamat Petang",
+  "Good Night": "Selamat Malam",
+};
+
+const introVocab = {
+  /*
+    I work at/as...
+    */
+  "How are you?": "Apa khabar?",
+  "Khabar baik, terima kasih": "I'm well, thank you",
+  // What's your name?
+  "My name is...": "Nama saya...",
+  "What is your full name?": "Apa nama penuh awak?",
+  "My full name is...": "Nama penuh saya...",
+  "You can call me...": "Awak boleh panggil saya...",
+  "How old are you?": "Berapa umur kamu",
+  // I am X years old
+  "Where are you from?": "Kamu dari mana?",
+  "I am from Kuala Lumpur": "Saya dari Kuala Lumpur",
+  "I come from Kuala Lumpur": "Saya datang dari Kuala Lumpur",
+};
