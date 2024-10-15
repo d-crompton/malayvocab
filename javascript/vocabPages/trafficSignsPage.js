f69b6b5d1a4b3fd38659d6e00012ae9ab5cceaf9
d-crompton <danny.crompton@live.co.uk>
2024-10-15 19:06:26 +0800

    .

---
+++ javascript/vocabPages/trafficSignsPage.js
@@ -0,0 +1,24 @@
+import { populateTable } from "../support.js";
+
+// Put Vocab in alphabetical order of the malay words
+const trafficSignsVocab = {
+  Awas: "Beware",
+  "Awas Banjir": "Flood Warning (<em>lit. Beware Flood</em>)",
+  Berhenti: "Stop",
+  "Beri Laluan": "Give Way",
+  "Bersedia Untuk Berhenti": "Prepare to Stop",
+  "Km/J<br>(Kilometer Sejam)": "Kilometer per Hour",
+  "Kawasan Sekolah Berakhir": "School Zone Ends",
+  "Kawasan Sekolah Bermula": "School Zone Starts",
+  "Ikut Kiri Kecuali Semasa Momotong":
+    "Stay Left Unless When Overtaking <br>(<em>Momotong also translates as 'cutting'</em>)",
+  Keluar: "Exit",
+  "Kenderaan Berat Berpusing Di Hadapan": "Heavy Vehicles Turning Ahead",
+  "Kurangkan Laju": "Reduce Speed",
+  Masuk: "Enter",
+  Perlahan: "Slow Down",
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  populateTable("tblTrafficSigns", trafficSignsVocab);
+});
